e11da21210405b7044c8a6949db719c4a1955c24
Phonlapha Tabwongsri <phonlaphat9826@gmail.com>
2022-07-11 16:52:59 +0700

    up

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require('http')
 const msg = require('./message.js')
+const {loadUser} = require('./user_models')
 
 const host = "localhost"
 const PORT = process.env.PORT || 5000
 const server = http.createServer((req,res)=>{
+    const jsonContent = JSON.stringify(loadUser())
     res.writeHead(200,{"Content-Type":"Text/plain"})
-    res.write(`${msg.message}`)
+    res.write(`${jsonContent}`)
     res.end()
 })
 
