aa2d6dbf3722bb5fabc7f624e572c4c36b78d458
Phonlapha Tabwongsri <phonlaphat9826@gmail.com>
2022-07-11 17:43:25 +0700

    up

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const http = require('http')
 const msg = require('./message.js')
-const {loadUser} = require('./user_models')
+// const {loadUser} = require('./user_model')
+const {loadVnl2022} = require('./vnl2022_model.js')
 
 const host = "localhost"
 const PORT = process.env.PORT || 5000
-const server = http.createServer((req,res)=>{
-    const jsonContent = JSON.stringify(loadUser())
-    res.writeHead(200,{"Content-Type":"Text/plain"})
+
+const server = http.createServer((req,res) => {
+    // const jsonContent = JSON.stringify(loadUser())
+    const jsonContent = JSON.stringify(loadVnl2022())
+    res.writeHead(200, { 'Content-Type' : 'text/plain' })
     res.write(`${jsonContent}`)
     res.end()
 })
 
-server.listen(PORT,()=>{
-    console.log(`Start server runnibg at http://${host}:${PORT}`)
+server.listen(PORT, ()=>{
+    console.log(`Start server runnig at http://${host}:${PORT}`)
 })
